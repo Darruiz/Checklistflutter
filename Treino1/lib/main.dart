bb3c694865bd7ac350b05f0f89a444e9ccdbecd7
Darruiz <darruizhomeoffice@gmail.com>
2023-06-23 17:24:23 -0300

    Prepare para add e inicio de historico

--- Treino1/lib/main.dart
+++ Treino1/lib/main.dart
@@ -25,8 +25,14 @@ class CalculadoraApp extends StatelessWidget {
   }
 }
 
-class Calculadora extends StatelessWidget {
+class Calculadora extends StatefulWidget {
+  @override
+  _CalculadoraState createState() => _CalculadoraState();
+}
+
+class _CalculadoraState extends State<Calculadora> {
   final TextEditingController _displayController = TextEditingController();
+  List<String> historico = [];
 
   @override
   Widget build(BuildContext context) {
@@ -39,33 +45,50 @@ class Calculadora extends StatelessWidget {
       body: Column(
         children: [
           Expanded(
-            child: Container(
-              color: Color.fromARGB(255, 0, 0, 0),
-              padding: EdgeInsets.all(16.0),
-              alignment: Alignment.bottomRight,
-              child: TextField(
-                controller: _displayController,
-                enabled: false,
-                textAlign: TextAlign.right,
-                style: TextStyle(
-                    fontSize: 48.0, color: const Color.fromARGB(255, 0, 0, 0)),
-                decoration: InputDecoration(
-                  border: InputBorder.none,
-                  contentPadding: EdgeInsets.zero,
-                ),
+            child: ListView.builder(
+              reverse: true,
+              itemCount: historico.length,
+              itemBuilder: (context, index) {
+                return ListTile(
+                  title: Text(
+                    historico[index],
+                    style: TextStyle(fontSize: 20.0, color: Colors.white),
+                  ),
+                );
+              },
+            ),
+          ),
+          Container(
+            color: Colors.black,
+            padding: EdgeInsets.all(16.0),
+            alignment: Alignment.bottomRight,
+            child: TextField(
+              controller: _displayController,
+              enabled: false,
+              textAlign: TextAlign.right,
+              style: TextStyle(fontSize: 48.0, color: Colors.white),
+              decoration: InputDecoration(
+                border: InputBorder.none,
+                contentPadding: EdgeInsets.zero,
               ),
             ),
           ),
           Expanded(
-            flex: 4,
+            flex: 2,
             child: SimpleCalculator(
+              onExpressionChanged: (expression) {
+                // Atualizar o histórico de operações
+                setState(() {
+                  historico.insert(0, expression);
+                });
+              },
               theme: const CalculatorThemeData(
-                displayColor: Color.fromARGB(255, 0, 0, 0),
-                displayStyle: const TextStyle(
-                    fontSize: 48.0, color: Color.fromARGB(255, 255, 255, 255)),
+                displayColor: Colors.white,
+                displayStyle:
+                    const TextStyle(fontSize: 48.0, color: Colors.white),
                 expressionColor: Colors.white,
-                expressionStyle: const TextStyle(
-                    fontSize: 20.0, color: Color.fromARGB(255, 0, 0, 0)),
+                expressionStyle:
+                    const TextStyle(fontSize: 20.0, color: Colors.white),
                 operatorColor: Colors.purple,
                 operatorStyle:
                     const TextStyle(fontSize: 24.0, color: Colors.white),
