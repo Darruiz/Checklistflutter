e52215ff480082bbe434c9dff098221fec0e0a73
Darruiz <darruizhomeoffice@gmail.com>
2023-06-23 18:51:13 -0300

    Remoção de estrutura por conta do teste em hot reload que bugou após algumas alterações básicas

--- Checklist/lib/main.dart
+++ Checklist/lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:intl/intl.dart';
 
 void main() {
   runApp(ChecklistApp());
@@ -24,11 +23,11 @@ class ChecklistScreen extends StatefulWidget {
 }
 
 class _ChecklistScreenState extends State<ChecklistScreen> {
-  List<Task> _tasks = [];
+  List<String> _tasks = [];
 
-  void _addTask(String title, DateTime date) {
+  void _addTask(String task) {
     setState(() {
-      _tasks.add(Task(title: title, date: date));
+      _tasks.add(task);
     });
   }
 
@@ -36,16 +35,6 @@ class _ChecklistScreenState extends State<ChecklistScreen> {
     setState(() {
       _tasks.removeAt(index);
     });
-  }
-
-  Future<DateTime> _selectDate(BuildContext context) async {
-    final DateTime picked = await showDatePicker(
-      context: context,
-      initialDate: DateTime.now(),
-      firstDate: DateTime(2000),
-      lastDate: DateTime(2100),
-    );
-    return picked;
   }
 
   @override
@@ -69,8 +58,7 @@ class _ChecklistScreenState extends State<ChecklistScreen> {
               itemBuilder: (context, index) {
                 final task = _tasks[index];
                 return ListTile(
-                  title: Text(task.title),
-                  subtitle: Text(DateFormat('dd/MM/yyyy').format(task.date)),
+                  title: Text(task),
                   trailing: IconButton(
                     icon: Icon(Icons.delete),
                     onPressed: () => _removeTask(index),
@@ -81,33 +69,26 @@ class _ChecklistScreenState extends State<ChecklistScreen> {
           ),
           Padding(
             padding: const EdgeInsets.all(16.0),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
+            child: Row(
               children: [
-                Text(
-                  'Add a Task',
-                  style: TextStyle(
-                    fontSize: 16,
-                    fontWeight: FontWeight.bold,
+                Expanded(
+                  child: TextField(
+                    maxLines: null, // Permite várias linhas
+                    textInputAction: TextInputAction.done,
+                    onSubmitted: _addTask,
+                    decoration: InputDecoration(
+                      hintText: 'Add a task',
+                    ),
                   ),
                 ),
-                SizedBox(height: 8.0),
+                SizedBox(width: 16.0),
                 ElevatedButton(
-                  onPressed: () async {
-                    final DateTime selectedDate = await _selectDate(context);
-                    if (selectedDate != null) {
-                      final String formattedDate =
-                          DateFormat('dd/MM/yyyy').format(selectedDate);
-                      final String newTask = await showDialog(
-                        context: context,
-                        builder: (context) => AddTaskDialog(),
-                      );
-                      if (newTask != null) {
-                        _addTask(newTask, selectedDate);
-                      }
-                    }
+                  onPressed: () {
+                    final newTask =
+                        'New Task'; // Replace with text from TextField
+                    _addTask(newTask);
                   },
-                  child: Text('Add Task'),
+                  child: Text('Add'),
                 ),
               ],
             ),
@@ -115,57 +96,5 @@ class _ChecklistScreenState extends State<ChecklistScreen> {
         ],
       ),
     );
-  }
-}
-
-class Task {
-  final String title;
-  final DateTime date;
-
-  Task({required this.title, required this.date});
-}
-
-class AddTaskDialog extends StatefulWidget {
-  @override
-  _AddTaskDialogState createState() => _AddTaskDialogState();
-}
-
-class _AddTaskDialogState extends State<AddTaskDialog> {
-  final TextEditingController _textController = TextEditingController();
-
-  @override
-  Widget build(BuildContext context) {
-    return AlertDialog(
-      title: Text('Add Task'),
-      content: TextField(
-        controller: _textController,
-        decoration: InputDecoration(
-          hintText: 'Enter task title',
-        ),
-      ),
-      actions: [
-        TextButton(
-          onPressed: () {
-            Navigator.of(context).pop();
-          },
-          child: Text('Cancel'),
-        ),
-        ElevatedButton(
-          onPressed: () {
-            final String newTask = _textController.text.trim();
-            if (newTask.isNotEmpty) {
-              Navigator.of(context).pop(newTask);
-            }
-          },
-          child: Text('Add'),
-        ),
-      ],
-    );
-  }
-
-  @override
-  void dispose() {
-    _textController.dispose();
-    super.dispose();
   }
 }
